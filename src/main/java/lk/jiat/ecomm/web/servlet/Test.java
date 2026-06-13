fbc958a6d2426604d1d1a6ec92fab35fbc7596e1
chanuka-rukshan <chanukarukshan81@gmail.com>
2026-06-13 15:44:27 +0530

    Done

--- src/main/java/lk/jiat/ecomm/web/servlet/Test.java
+++ src/main/java/lk/jiat/ecomm/web/servlet/Test.java
@@ -5,13 +5,30 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ecomm.user.dto.UserDTO;
+import lk.jiat.ecomm.user.remote.UserRemote;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/test")
 public class Test extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().write("Ecomm Web module Test");
+
+        try {
+            InitialContext ic = new InitialContext();
+            UserRemote userRemote = (UserRemote) ic.lookup("java:global/ecomm-user-1.0/UserSessionBeen");
+            List<UserDTO> allUsers = userRemote.getAllUsers();
+            for (UserDTO user : allUsers) {
+                user.toString();
+            }
+
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
